9188446662987b3fa730229e69af6560e4f3548e
frank <cwj459378216@163.com>
2021-04-11 13:27:41 +0800

    add logout

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,9 +1,18 @@
 <template>
-  <div>home</div>
+  <div>
+    <el-button type="info" @click="logout"></el-button>
+  </div>
 </template>
 
 <script>
-export default { }
+export default {
+  methods: {
+    logout () {
+      window.sessionStorage.clear()
+      this.$route.push('/login')
+    }
+  }
+}
 </script>
 
 <style lang="less" scoped>
